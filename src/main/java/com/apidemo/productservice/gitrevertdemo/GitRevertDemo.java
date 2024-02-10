71c18b6b9d55928d7ace37b16b4d05dd403d0f81
Mahesh <maheshphutane1810@gmail.com>
2024-02-10 20:08:37 +0530

    Git revert Demo 1st commit

---
+++ src/main/java/com/apidemo/productservice/gitrevertdemo/GitRevertDemo.java
@@ -0,0 +1,7 @@
+package com.apidemo.productservice.gitrevertdemo;
+
+public class GitRevertDemo {
+    public static void main(String[] args) {
+        System.out.println("This is my First Commit");
+    }
+}
