b5831a29241faeb8178f8a4c9212d1ae9b705a8f
Mahesh <maheshphutane1810@gmail.com>
2024-02-10 20:36:49 +0530

    Git revert Demo 2nd commit

--- src/main/java/com/apidemo/productservice/gitrevertdemo/GitRevertDemo.java
+++ src/main/java/com/apidemo/productservice/gitrevertdemo/GitRevertDemo.java
@@ -3,5 +3,6 @@ package com.apidemo.productservice.gitrevertdemo;
 public class GitRevertDemo {
     public static void main(String[] args) {
         System.out.println("This is my 1st commit");
+        System.out.println("This is my 2nd Commit");
     }
 }
