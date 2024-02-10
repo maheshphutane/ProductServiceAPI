8208bef881b043d3023d53c0bfd033d9d0ec1c4f
Mahesh <maheshphutane1810@gmail.com>
2024-02-10 20:37:40 +0530

    Git revert Demo 3rd commit

--- src/main/java/com/apidemo/productservice/gitrevertdemo/GitRevertDemo.java
+++ src/main/java/com/apidemo/productservice/gitrevertdemo/GitRevertDemo.java
@@ -4,5 +4,6 @@ public class GitRevertDemo {
     public static void main(String[] args) {
         System.out.println("This is my 1st commit");
         System.out.println("This is my 2nd Commit");
+        System.out.println("This is my 3rd commit");
     }
 }
